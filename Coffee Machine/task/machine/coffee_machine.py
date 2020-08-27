d4cb90e0cf4ddae08a1040e7a55b7dd099abaf81
Suraj Wate <suraj.wate@gmail.com>
2020-08-27 18:56:32 +0530

    Stage #2: Machines have needs

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -7,3 +7,10 @@ Pouring coffee into the cup
 Pouring some milk into the cup
 Coffee is ready!
 """)
+
+coffee_required = int(input("Write how many cups of coffee you will need:"))
+print("""
+For {} cups of coffee you will need:
+{} ml of water
+{} ml of milk
+{} g of coffee beans""".format(coffee_required, coffee_required*200, coffee_required*50, coffee_required*15))
