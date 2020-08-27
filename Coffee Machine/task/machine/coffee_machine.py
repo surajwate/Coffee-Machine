7ddca2158a2d888b3ff7f29d6bc7d203bb95930c
Suraj Wate <suraj.wate@gmail.com>
2020-08-27 23:34:47 +0530

    Stage 4/6: Action! One of the cleanest code I have written.

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,14 +1,54 @@
-water = int(input("Write how many ml of water the coffee machine has:"))
-milk = int(input("Write how many ml of milk the coffee machine has:"))
-beans = int(input("Write how many grams of coffee beans the coffee machine has:"))
-coffee_require = int(input("Write how many cups of coffee you will need:"))
+machine_content = {"water": 400, "milk": 540, "beans": 120, "cups": 9, "money": 550}
 
-coffee_make = min([water//200, milk//50, beans//15])
 
-if coffee_make == coffee_require:
-    print("Yes, I can make that amount of coffee")
-elif coffee_make < coffee_require:
-    print("No, I can make only {} cups of coffee".format(coffee_make))
-else:
-    print("Yes, I can make that amount of coffee (and even {} more than that)".format(coffee_make-coffee_require))
+def status_display(content):
+    print("""
+The coffee machine has:
+{} of water
+{} of milk
+{} of coffee beans
+{} of disposable cups
+{} of money\n""".format(*list(content.values())))
 
+
+def refill():
+    refilled = {"water": int(input("Write how many ml of water do you want to add:")),
+                "milk": int(input("Write how many ml of milk do you want to add:")),
+                "beans": int(input("Write how many grams of coffee beans do you want to add:")),
+                "cups": int(input("Write how many disposable cups of coffee do you want to add:"))}
+    return refilled
+
+
+def update(content, updated_content) -> dict:
+    for keys in updated_content:
+        content[keys] = content[keys] + updated_content[keys]
+    return content
+
+
+def coffee_selection(ans: str) -> dict:
+    espresso = {"water": -250, "beans": -16, "cups": -1, "money": 4}
+    latte = {"water": -350, "milk": -75, "beans": -20, "cups": -1, "money": 7}
+    cappuccino = {"water": -200, "milk": -100, "beans": -12, "cups": -1, "money": 6}
+    if ans == "1":
+        return espresso
+    elif ans == "2":
+        return latte
+    elif ans == "3":
+        return cappuccino
+
+
+status_display(machine_content)
+action = input("Write action (buy, fill, take):")
+
+if action == "buy":
+    answer = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    coffee_selected = coffee_selection(answer)
+    machine_content = update(machine_content, coffee_selected)
+elif action == "fill":
+    refill_content = refill()
+    machine_content = update(machine_content, refill_content)
+elif action == "take":
+    print("I gave you ${}".format(machine_content["money"]))
+    machine_content = update(machine_content, {"money": -machine_content["money"]})
+
+status_display(machine_content)
